d6171757ea8a6ed80cb4bbc83705de7a648f5b9b
Cortana <jian635446559@gmail.com>
2022-03-08 18:33:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
     if (!isset($_SERVER['PHP_AUTH_USER'])) {
         header('WWW-Authenticate: Basic realm="My Realm"');
         header('HTTP/1.0 401 Unauthorized');
         echo 'Text to send if user hits Cancel button';
         exit;
-      } else {
-        echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
-        echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
-      }
+    } else {
+        if ($_SERVER['PHP_AUTH_USER'] != 'admin' && $_SERVER['PHP_AUTH_PW'] != 'Aa111111') {
+?>
+            <form action="upload" enctype="multipart/form-data">
+                <input type="file" name="file">
+                <input type="submit" value="提交">
+            </form>
+<?php
+        } else {
+?>
+            <p>认证失败</p>
+<?php
+        }
+    }
+?>
+</body>
+</html>
+<?php
+    function upload() {
+        var_dump($_FILES['file']);
+    }
+?>
